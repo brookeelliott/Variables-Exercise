e5dd5a5c4d394b1621d547868dcaf08a24449995
Brooke Elliott <brookeelliott711@gmail.com>
2023-04-11 16:12:25 -0600

    Brooke Variables Exercise

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,23 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            // Step 1:
+            string dinnerType = "Tacos";
+            int tacoCount = 3;
+            char eatTacos = 'y';
+            bool initiateEating = true;
+            double price = 2.0;
+            decimal bundlePrice = 1.5m;
+
+
+            // Step 2:
+            Console.WriteLine($"Tonight I want to eat {dinnerType}.\n" +
+                              $"I plan to eat {tacoCount} tacos.\n" +
+                              $"y/n should I start eating now? ... {eatTacos}.\n" +
+                              $"System check: Am I clear to start eating? {initiateEating}.\n" +
+                              $"Note: Tacos cost {price} if you buy them individually.\n" +
+                              $"Note: Tacos cost {bundlePrice} if you buy them 3 at a time.\n");
         }
     }
 }
